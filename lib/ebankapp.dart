746c38d9f94b1aff16e082c12f23f308f4c04333
Wojtek Bednarzak <wojtek.bednarzak@gmail.com>
2019-04-19 00:27:14 +0100

    Add basic AngularDart web frontend.
    This is in no way complete. This is just the base for the future
    changes. It is possible that this codebase wouldn't even live under the
    home page and would instead be ran from a subdomain.

---
+++ lib/ebankapp.dart
@@ -0,0 +1,9 @@
+import 'package:angular/angular.dart';
+
+@Component(
+    selector: 'ebank-app',
+    templateUrl: './ebankapp.html',
+    styleUrls: const ['./ebankapp.css'])
+class EbankAppComponent {
+  var darkTheme = false;
+}
